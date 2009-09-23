98cb713dd0bc0dc4499766feb28d0c7bbb98fba3
Min Huang <min.huang@alumni.usc.edu>
2009-09-23 13:19:57 -0700

    Dependemency management added

--- lib/jsdm/preprocessor.rb
+++ lib/jsdm/preprocessor.rb
@@ -4,11 +4,21 @@ module JSDM
 
     def initialize(source_root)
       self.source_root = source_root
-      self.nodes = []
-      self.arcs = []
+      self.sources = Dir["#{source_root}/**/*.js"]
+      self.dependencies = []
     end
 
-    def parse_include(file)
+    def dependency_graph
+      sources.each do |source|
+        includes = parse_includes(source)
+        resolve_dependencies(includes).each do |dep|
+          dependencies << [source, dep]
+        end
+      end
+      DirectedGraph.new(sources, dependecies)
+    end
+
+    def parse_includes(file)
       pattern = /^\s*\/\/\s*#include\s*/
       File.new(file).
            readlines.
@@ -16,19 +26,20 @@ module JSDM
            map { |line| line.sub(pattern, "").strip }
     end
 
-    def resolve_include(entries)
-      sources = []
-      entries.each do |entry|
-        entry.split(",").each do |file_pattern|
-          sources += Dir["#{source_root}/#{file_pattern.strip}"]
+    def resolve_dependencies(includes)
+      deps = []
+      includes.each do |inc|
+        inc.split(",").each do |entry|
+          deps += Dir["#{source_root}/#{entry.strip}"]
         end
       end
-      sources
+      deps 
     end
 
     private
     attr_writer :source_root
     attr_accessor :nodes
     attr_accessor :arcs
+    attr_reader :sources
   end
 end
