c829e00eff5754402abcd10444804a9966b9211d
Min Huang <min.huang@alumni.usc.edu>
2009-09-23 16:20:09 -0700

    More tests passing.

--- test/jsdm/depth_first_search_test.rb
+++ test/jsdm/depth_first_search_test.rb
@@ -27,14 +27,33 @@ class DepthFirstSearchTest < Test::Unit::TestCase
              [8, 9],
              [8, 12],
              [9, 10],
-             [9, 11]].to_set
+             [9, 11]]
     @graph = DirectedGraph.new(@nodes, @arcs)
   end
 
-  def test_forward_edges
+  def test_find_tree_edges
+    result = dfs(@graph)
+    assert_equal @arcs.to_set, result[:tree_edges].to_set
+  end
+
+  def test_find_forward_edges
     forward_edges = [[1, 4], [8, 10]]
     @graph = DirectedGraph.new(@nodes, @arcs + forward_edges)
     result = dfs(@graph)
     assert_equal forward_edges.to_set, result[:forward_edges].to_set
+  end
+
+  def test_find_back_edges
+    back_edges = [[5, 2], [11, 8]]
+    @graph = DirectedGraph.new(@nodes, @arcs + back_edges)
+    result = dfs(@graph)
+    assert_equal back_edges.to_set, result[:back_edges].to_set
+  end
+
+  def test_find_cross_edges
+    cross_edges = [[10, 5], [9, 6]]
+    @graph = DirectedGraph.new(@nodes, @arcs + cross_edges)
+    result = dfs(@graph)
+    assert_equal cross_edges.to_set, result[:cross_edges].to_set
   end
 end
