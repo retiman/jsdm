8b94c5fce8bdabf5ce48122426db3bcd9c7a27c9
Min Huang <min.huang@alumni.usc.edu>
2009-09-23 18:22:53 -0700

    Entry point into program

--- lib/jsdm.rb
+++ lib/jsdm.rb
@@ -1,2 +1,15 @@
-module JSDM
+require 'jsdm/preprocessor'
+
+class Jsdm
+  def initialize(source_root)
+    self.source_root = source_root
+  end
+
+  def sort
+    preprocessor = JSDM::Preprocessor.new(source_root)
+    preprocessor.process
+  end
+
+  attr_accessor :source_root
+  private :source_root=
 end
