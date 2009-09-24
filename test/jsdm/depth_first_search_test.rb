41e428fb337e38e47a794a79ba52c5a9d0364e9f
Min Huang <min.huang@alumni.usc.edu>
2009-09-23 18:51:41 -0700

    Added test for topological sort

--- test/jsdm/depth_first_search_test.rb
+++ test/jsdm/depth_first_search_test.rb
@@ -56,4 +56,25 @@ class DepthFirstSearchTest < Test::Unit::TestCase
     result = dfs(@graph)
     assert_equal cross_edges.to_set, result[:cross_edges].to_set
   end
+
+  def test_topological_sort
+    # make sure the sort happens regardless of node order
+    [[11, 6, 1, 7, 3, 8, 5, 9, 4, 10, 2],
+     [2, 8, 11, 5, 1, 9, 3, 4, 7, 10, 6],
+     [6, 9, 2, 3, 11, 4, 1, 7, 5, 10, 8],
+     [8, 4, 6, 1, 2, 7, 11, 5, 3, 10, 9],
+     [9, 7, 8, 11, 6, 5, 2, 3, 1, 10, 4],
+     [4, 5, 9, 2, 8, 3, 6, 1, 11, 10, 7],
+     [7, 3, 4, 6, 9, 1, 8, 11, 2, 10, 5],
+     [5, 1, 7, 8, 4, 11, 9, 2, 6, 10, 3],
+     [3, 11, 5, 9, 7, 2, 4, 6, 8, 10, 1],
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]].each do |nodes|
+      @nodes = nodes
+      @graph = DirectedGraph.new(@nodes, @arcs)
+      result = dfs(@graph)
+      # intersection via & preserves order
+      # arc.first should come before arc.last after a topological sort
+      @arcs.each { |arc| assert_equal arc, (result[:sorted] & arc) }
+    end
+  end
 end
