52fa06b3766211941511b911781589c53dd62305
Min Huang <min.huang@alumni.usc.edu>
2010-01-25 17:07:20 -0800

    Use single tick instead

--- test/jsdm/dependency_manager_test.rb
+++ test/jsdm/dependency_manager_test.rb
@@ -1,17 +1,17 @@
 require 'set'
-require "jsdm/dependency_manager"
-require "test/unit"
+require 'jsdm/dependency_manager'
+require 'test/unit'
 
 class DependencyManagerTest < Test::Unit::TestCase
   def test_manager_process
     sources = %w(c.js a/b.js b/c/d/e.js b/c/d.js b/c.js)
-    dependencies = [["b/c.js",     "a/b.js"],
-                    ["c.js",       "a/b.js"],
-                    ["b/c/d.js",   "b/c.js"],
-                    ["b/c/d/e.js", "a/b.js"],
-                    ["b/c/d/e.js", "b/c/d.js"],
-                    ["b/c/d/e.js", "b/c.js"],
-                    ["b/c/d/e.js", "c.js"]]  
+    dependencies = [['b/c.js',     'a/b.js'],
+                    ['c.js',       'a/b.js'],
+                    ['b/c/d.js',   'b/c.js'],
+                    ['b/c/d/e.js', 'a/b.js'],
+                    ['b/c/d/e.js', 'b/c/d.js'],
+                    ['b/c/d/e.js', 'b/c.js'],
+                    ['b/c/d/e.js', 'c.js']]  
     manager = JSDM::DependencyManager.new sources
     dependencies.each do |arc|
       manager.add_dependency arc.first, arc.last
@@ -24,13 +24,13 @@ class DependencyManagerTest < Test::Unit::TestCase
 
   def test_dependencies_of
     sources = %w(c.js a/b.js b/c/d/e.js b/c/d.js b/c.js)
-    dependencies = [["b/c.js",     "a/b.js"],
-                    ["c.js",       "a/b.js"],
-                    ["b/c/d.js",   "b/c.js"],
-                    ["b/c/d/e.js", "a/b.js"],
-                    ["b/c/d/e.js", "b/c/d.js"],
-                    ["b/c/d/e.js", "b/c.js"],
-                    ["b/c/d/e.js", "c.js"]]  
+    dependencies = [['b/c.js',     'a/b.js'],
+                    ['c.js',       'a/b.js'],
+                    ['b/c/d.js',   'b/c.js'],
+                    ['b/c/d/e.js', 'a/b.js'],
+                    ['b/c/d/e.js', 'b/c/d.js'],
+                    ['b/c/d/e.js', 'b/c.js'],
+                    ['b/c/d/e.js', 'c.js']]  
     manager = JSDM::DependencyManager.new sources
     dependencies.each do |arc|
       manager.add_dependency arc.first, arc.last
