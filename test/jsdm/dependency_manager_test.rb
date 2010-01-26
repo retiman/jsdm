0f2e910fc7dcf0e79281113fab3c30c86028c6d4
Min Huang <min.huang@alumni.usc.edu>
2010-01-25 17:12:41 -0800

    Factored out test fixtures

--- test/jsdm/dependency_manager_test.rb
+++ test/jsdm/dependency_manager_test.rb
@@ -3,36 +3,31 @@ require 'jsdm/dependency_manager'
 require 'test/unit'
 
 class DependencyManagerTest < Test::Unit::TestCase
+  def setup
+    @sources = %w(c.js a/b.js b/c/d/e.js b/c/d.js b/c.js)
+    @dependencies = [['b/c.js',     'a/b.js'],
+                     ['c.js',       'a/b.js'],
+                     ['b/c/d.js',   'b/c.js'],
+                     ['b/c/d/e.js', 'a/b.js'],
+                     ['b/c/d/e.js', 'b/c/d.js'],
+                     ['b/c/d/e.js', 'b/c.js'],
+                     ['b/c/d/e.js', 'c.js']]
+  end
+
   def test_process
-    sources = %w(c.js a/b.js b/c/d/e.js b/c/d.js b/c.js)
-    dependencies = [['b/c.js',     'a/b.js'],
-                    ['c.js',       'a/b.js'],
-                    ['b/c/d.js',   'b/c.js'],
-                    ['b/c/d/e.js', 'a/b.js'],
-                    ['b/c/d/e.js', 'b/c/d.js'],
-                    ['b/c/d/e.js', 'b/c.js'],
-                    ['b/c/d/e.js', 'c.js']]
-    manager = JSDM::DependencyManager.new sources
-    dependencies.each do |arc|
+    manager = JSDM::DependencyManager.new @sources
+    @dependencies.each do |arc|
       manager.add_dependency arc.first, arc.last
     end
     sorted = manager.process
-    dependencies.each do |arc|
+    @dependencies.each do |arc|
       assert_equal arc.reverse, (sorted & arc.reverse)
     end
   end
 
   def test_dependencies_of
-    sources = %w(c.js a/b.js b/c/d/e.js b/c/d.js b/c.js)
-    dependencies = [['b/c.js',     'a/b.js'],
-                    ['c.js',       'a/b.js'],
-                    ['b/c/d.js',   'b/c.js'],
-                    ['b/c/d/e.js', 'a/b.js'],
-                    ['b/c/d/e.js', 'b/c/d.js'],
-                    ['b/c/d/e.js', 'b/c.js'],
-                    ['b/c/d/e.js', 'c.js']]
-    manager = JSDM::DependencyManager.new sources
-    dependencies.each do |arc|
+    manager = JSDM::DependencyManager.new @sources
+    @dependencies.each do |arc|
       manager.add_dependency arc.first, arc.last
     end
     sorted = manager.process
