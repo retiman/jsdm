29d7369a34941fe8edbe1ee3c0e77374a2fa3f2d
Min Huang <min.huang@alumni.usc.edu>
2010-01-25 16:11:40 -0800

    Whitespace

--- test/jsdm_test.rb
+++ test/jsdm_test.rb
@@ -8,29 +8,29 @@ class JSDMTest < Test::Unit::TestCase
   def setup
     @root = "test/res/jsdm/"
   end
-  
+
   def make(dir)
     @root += dir
     jsdm = JSDM.new @root
     jsdm.process!
     jsdm
   end
-  
+
   def test_no_sources
     jsdm = make(__method__.to_s)
     assert_equal [], jsdm.sources
   end
-  
+
   def test_single_source_file
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
 
-  def test_require_self  
+  def test_require_self
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
-  
+
   def test_require_missing
     assert_raise JSDM::UnsatisfiableDependencyError do
       jsdm = make(__method__.to_s)
@@ -41,12 +41,12 @@ class JSDMTest < Test::Unit::TestCase
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/b.js"].to_set, jsdm.sources.to_set
   end
-  
+
   def test_one_depends_on_other
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/b.js"], jsdm.sources
   end
-  
+
   def test_circular_with_2
     begin
       jsdm = make(__method__.to_s)
@@ -66,10 +66,10 @@ class JSDMTest < Test::Unit::TestCase
       assert_equal expected.to_set, e.deps
     end
   end
-  
+
   def test_complex_dependency
     jsdm = make(__method__.to_s)
-    assert_equal ["#{@root}/a.js", "#{@root}/b.js", "#{@root}/c.js"], 
+    assert_equal ["#{@root}/a.js", "#{@root}/b.js", "#{@root}/c.js"],
                  jsdm.sources
   end
 
@@ -78,30 +78,30 @@ class JSDMTest < Test::Unit::TestCase
     assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
-  
+
   def test_down_two_dir
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
-  
+
   def test_up_one_dir
     jsdm = make(__method__.to_s)
-    assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set, 
+    assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
-  
+
   def test_up_two_dir
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/i/b.js"].to_set,
                  jsdm.sources.to_set
   end
-  
+
   def test_glob_matches_self
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
-  
+
   def test_circular_with_glob_2
     begin
       jsdm = make(__method__.to_s)
@@ -110,7 +110,7 @@ class JSDMTest < Test::Unit::TestCase
       assert_equal expected, e.deps
     end
   end
-  
+
   def test_double_star_with_2
     jsdm = make(__method__.to_s)
     assert_equal ["#{@root}/I/b.js", "#{@root}/a.js"].to_set,
@@ -126,7 +126,7 @@ class JSDMTest < Test::Unit::TestCase
     expected = "// test/res/jsdm/test_concatenation/b.js:\n\n" +
                "// test/res/jsdm/test_concatenation/a.js:\n" +
                "// #require b.js\n"
-    result = File.new("tmp/test_concatenation").read 
+    result = File.new("tmp/test_concatenation").read
     assert_equal expected, result
   end
 end
