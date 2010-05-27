603253f0e5e183d2f700ae85c158cc64f9274676
Min Huang <min.huang@alumni.usc.edu>
2010-05-26 23:19:56 -0700

    There seems to be no reason to do this

--- test/run_tests.rb
+++ test/run_tests.rb
@@ -6,5 +6,4 @@ base_dir = File.expand_path(File.join(test_dir, '..'))
 $: << File.join(base_dir, 'lib')
 
 files = Dir[File.join(test_dir, '/**/*_test.rb')]
-files.delete_if { |name| name =~ /.*_integration_test.rb$/ }
 files.each &method(:require)
