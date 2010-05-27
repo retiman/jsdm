6b04822df054973778f7e9011ec0667160b88e4e
Min Huang <min.huang@alumni.usc.edu>
2010-05-26 23:25:35 -0700

    Platform neutral File.join

--- test/jsdm_test.rb
+++ test/jsdm_test.rb
@@ -6,48 +6,48 @@ class JSDMTest < Test::Unit::TestCase
   attr_accessor :root
 
   def setup
-    @root = 'test/res/jsdm/'
+    @root = File.join('test', 'res', 'jsdm')
   end
 
-  def make(dir)
-    @root += dir
+  def create_jsdm(dir)
+    @root = File.join(@root, dir)
     JSDM.new @root
   end
 
   def test_no_sources
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal [], jsdm.sources
   end
 
   def test_single_source_file
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
 
   def test_require_self
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
 
   def test_require_missing
     assert_raise JSDM::UnsatisfiableDependencyError do
-      jsdm = make(__method__.to_s)
+      jsdm = create_jsdm(__method__.to_s)
     end
   end
 
   def test_two_source_files
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/b.js"].to_set, jsdm.sources.to_set
   end
 
   def test_one_depends_on_other
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/b.js"], jsdm.sources
   end
 
   def test_circular_with_2
     begin
-      jsdm = make(__method__.to_s)
+      jsdm = create_jsdm(__method__.to_s)
     rescue JSDM::CircularDependencyError => e
       expected = [["#{@root}/a.js", "#{@root}/b.js"].to_set].to_set
       assert_equal expected, e.deps
@@ -56,7 +56,7 @@ class JSDMTest < Test::Unit::TestCase
 
   def test_circular_with_3
     begin
-      jsdm = make(__method__.to_s)
+      jsdm = create_jsdm(__method__.to_s)
     rescue JSDM::CircularDependencyError => e
       expected = [
         ["#{@root}/a.js", "#{@root}/b.js", "#{@root}/c.js"].to_set
@@ -66,43 +66,43 @@ class JSDMTest < Test::Unit::TestCase
   end
 
   def test_complex_dependency
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/b.js", "#{@root}/c.js"],
                  jsdm.sources
   end
 
   def test_down_one_dir
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
 
   def test_down_two_dir
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
 
   def test_up_one_dir
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/b.js"].to_set,
                  jsdm.sources.to_set
   end
 
   def test_up_two_dir
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js", "#{@root}/I/i/b.js"].to_set,
                  jsdm.sources.to_set
   end
 
   def test_glob_matches_self
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/a.js"], jsdm.sources
   end
 
   def test_circular_with_glob_2
     begin
-      jsdm = make(__method__.to_s)
+      jsdm = create_jsdm(__method__.to_s)
     rescue JSDM::CircularDependencyError => e
       expected = [["#{@root}/a.js", "#{@root}/b.js"].to_set].to_set
       assert_equal expected, e.deps
@@ -110,7 +110,7 @@ class JSDMTest < Test::Unit::TestCase
   end
 
   def test_double_star_with_2
-    jsdm = make(__method__.to_s)
+    jsdm = create_jsdm(__method__.to_s)
     assert_equal ["#{@root}/I/b.js", "#{@root}/a.js"].to_set,
                  jsdm.sources.to_set
   end
