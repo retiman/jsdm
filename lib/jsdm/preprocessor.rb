9887b5e19f9c0a4bef1dbc41dba96339b766257d
Min Huang <min.huang@alumni.usc.edu>
2010-05-30 21:27:22 -0700

    Define defaults on Preprocessor

--- lib/jsdm/preprocessor.rb
+++ lib/jsdm/preprocessor.rb
@@ -1,19 +1,23 @@
 class JSDM
   class Preprocessor
-    attr_accessor :sources, :comment_pattern, :require_pattern
+    attr_accessor :sources, :comment_pattern, :require_pattern, :options
 
-    def initialize(sources)
+
+    def initialize(sources, options = {})
+      defaults = {
+        :comment_pattern => Preprocessor.cp,
+        :require_pattern => Preprocessor.rp
+      }
       self.sources = sources
-      self.comment_pattern = /^\s*\/\/\s*/
-      self.require_pattern = /#{comment_pattern}#\s*require\s*/
+      self.options = defaults.merge options
     end
 
     def process_single(source)
       File.open(source).
            each_line.
-           take_while { |line| line =~ comment_pattern }.
-           select { |line| line =~ require_pattern }.
-           map { |line| line.sub!(require_pattern, "").split(",") }.
+           take_while { |line| line =~ options[:comment_pattern] }.
+           select { |line| line =~ options[:require_pattern] }.
+           map { |line| line.sub!(options[:require_pattern], "").split(",") }.
            flatten.
            map { |entry| entry.strip }
     end
@@ -21,5 +25,13 @@ class JSDM
     def process
       sources.map { |source| [source, process_single(source)] }
     end
+
+    class << self
+      attr_accessor :cp, :rp
+      private :cp=, :rp=
+    end
+
+    self.cp = /^\s*\/\/\s*/
+    self.rp = /#{self.cp}#\s*require\s*/
   end
 end
