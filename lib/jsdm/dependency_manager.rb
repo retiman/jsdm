4712ac77277007d54571cb1c3c6a3c62986eb7bc
Min Huang <min.huang@alumni.usc.edu>
2010-06-08 21:41:27 -0700

    Documentation for dependency_manager

--- lib/jsdm/dependency_manager.rb
+++ lib/jsdm/dependency_manager.rb
@@ -5,6 +5,8 @@ require 'jsdm/natural_loops'
 require 'jsdm/errors'
 
 class JSDM
+  # This class processes a dependency graph to find the order the source files
+  # should be in after a topological sort.
   class DependencyManager
     attr_accessor :sources, :dependencies, :graph
 
@@ -14,12 +16,15 @@ class JSDM
       self.graph = nil
     end
 
+    # Add a dependency to the dependency graph.
     def add_dependency(source, dependency)
       unless same_file? dependency, source
         dependencies << [dependency, source]
       end
     end
 
+    # Recursively calculate dependencies for an individual source file.  Uses
+    # the acc argument as an accumulator.
     def dependencies_for(source, acc = [])
        ds = dependencies.select { |d| d.last == source }.
                          map { |d| d.first }
@@ -29,6 +34,8 @@ class JSDM
        acc
     end
 
+    # Does a topological sort to find the order of dependencies, and checks
+    # for any circular dependencies.
     def process
       self.sources = sources.uniq.delete_if { |e| e.empty? }
       self.dependencies = dependencies.uniq.delete_if { |e| e.empty? }
@@ -41,6 +48,7 @@ class JSDM
 
     private
 
+    # Test if two files have resolved to the same thing
     def same_file?(a, b)
       File.expand_path(a) == File.expand_path(b)
     end
