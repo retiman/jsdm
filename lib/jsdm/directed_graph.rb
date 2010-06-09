8abfce5a6901f77959c54a9c581c9a86cd975fe3
Min Huang <min.huang@alumni.usc.edu>
2010-06-08 21:37:23 -0700

    Documentation for directed graph

--- lib/jsdm/directed_graph.rb
+++ lib/jsdm/directed_graph.rb
@@ -1,4 +1,7 @@
 class JSDM
+  # Very simple class to represent a directed graph using adjacency lists.
+  # See http://en.wikipedia.org/wiki/Digraph_(mathematics)
+  # See http://en.wikipedia.org/wiki/Adjacency_list
   class DirectedGraph
     attr_accessor :nodes, :arcs
     private :nodes=, :arcs=
@@ -8,6 +11,8 @@ class JSDM
       self.arcs  = arcs
     end
 
+    # Returns a list of nodes that are in the tail of an arc whose head
+    # is the given node.
     def successors(node)
       arcs.select { |a| a.first == node }.map { |a| a.last }
     end
