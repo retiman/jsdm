0b863ccb995293783ad9c2f27d41757acc95e9b3
Min Huang <min.huang@alumni.usc.edu>
2010-06-08 20:34:37 -0700

    Docs for errors

--- lib/jsdm/errors.rb
+++ lib/jsdm/errors.rb
@@ -1,6 +1,9 @@
 require 'pp'
 
 class JSDM
+  # This error is raised if a set of files (or if multiple sets of files) are
+  # in a circular dependency.  JSDM will report them all, so deps will be a
+  # set of sets.
   class CircularDependencyError < StandardError
     attr_accessor :deps
     private :deps=
@@ -15,6 +18,9 @@ class JSDM
     end
   end
 
+  # Used internally to indicate that a file could not be found; this error will
+  # not be exposed to the user.  This error eventually gets wrapped in a
+  # UnsatisfiableDependencyError.
   class FileNotFoundError < StandardError
     attr_accessor :file
     private :file=
@@ -25,6 +31,9 @@ class JSDM
     end
   end
 
+  # This error is raised if a file could not be found.  This error includes
+  # information about the dependency that couldn't be found, and the source
+  # that asked for that dependency.
   class UnsatisfiableDependencyError < StandardError
     attr_accessor :source, :dep
     private :source=, :dep=
